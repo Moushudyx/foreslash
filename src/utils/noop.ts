35d444e13439b3422442fcdb3986fbfa34adc2e9
moushu <moushudyx@163.com>
2024-09-02 18:34:57 +0800

    feat: 添加功能性函数 pass 和 passWith - 补充 pass 的测试，passWith 的还没有

--- src/utils/noop.ts
+++ src/utils/noop.ts
@@ -1,6 +1,34 @@
 /**
- *
+ * 不做任何操作，返回`void`，一般用于函数式编程
  */
-const noop: (...args: any[]) => void = /*#__PURE__*/ function noop() {};
+const noop: (...args: any[]) => void = /*#__PURE__*/ function noop() {}
 
-export { noop };
+/**
+ * 不做任何操作，返回第一个参数，一般用于函数式编程
+ * @param value 任意值
+ * @returns 传入的第一个值
+ */
+export function pass<T>(value: T, ...args: any[]): T;
+export function pass<T>(...args: T[]): T;
+export function pass(): undefined;
+export function pass<T>(value?: T): T | undefined {
+  return value
+}
+/**
+ * 接受副作用函数，返回一个带有副作用的函数，一般用于函数式编程
+ * @param fn 副作用函数，注意此函数返回值会被忽略
+ * @returns 相当于函数式编程中的`pass`，但是会调用副作用函数
+ * @example
+ * ```ts
+ * const passWithLog = passWith(console.log)
+ * const pipeWithLog = pipe( ...otherFunctions , passWithLog)
+ * ```
+ */
+export function passWith<T>(fn: (arg: T) => any): (arg: T) => T {
+  return (arg) => {
+    fn(arg)
+    return arg
+  }
+}
+
+export { noop }
--- test/utils/noop.test.ts
+++ test/utils/noop.test.ts
@@ -1,11 +1,22 @@
-import { noop } from "../../src/index";
+import { noop, pass } from '../../src/index'
 
 describe('noop', () => {
   it('不处理参数', () => {
-    const args = [1, 'two', true, null, undefined, {}, []];
-    expect(noop(...args)).toBe(undefined);
-    const spy = jest.fn();
-    noop(spy);
-    expect(spy).not.toHaveBeenCalled();
-  });
-});
+    const args = [1, 'two', true, null, undefined, {}, []]
+    expect(noop(...args)).toBe(undefined)
+    const spy = jest.fn()
+    noop(spy)
+    expect(spy).not.toHaveBeenCalled()
+  })
+})
+
+describe('pass', () => {
+  it('不处理参数', () => {
+    const args = [1, 'two', true, null, undefined, {}, []] as const
+    expect(pass(...args)).toBe(args[0])
+    expect(pass()).toBe(undefined)
+    const spy = jest.fn()
+    pass(spy)
+    expect(spy).not.toHaveBeenCalled()
+  })
+})
