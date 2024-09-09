e780e845a8c4c6516fb98839680dadc9f65f8c84
moushu <moushudyx@163.com>
2024-09-09 15:45:32 +0800

    fix: 调用 Object.prototype.toString 获取 tag

---
+++ src/utils/_getTag.ts
@@ -0,0 +1,8 @@
+const object2String = Object.prototype.toString
+/**
+ * 调用`Object.prototype.toString`获取对象类型名称
+ * @param value 要判断的类型
+ */
+export function getTag(value: unknown): string {
+  return object2String.call(value)
+}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,2 @@
+export * from './_getTag'
 export * from './_globalThis'
