4adfb0754c8712d5f302746a57d2dc51f3fa19b2
moushu <moushudyx@163.com>
2024-09-11 01:24:31 +0800

    test: 添加 passWith 测试

--- test/noop.test.ts
+++ test/noop.test.ts
@@ -1,4 +1,4 @@
-import { noop, pass } from '../src/index'
+import { noop, pass, passWith } from '../src/index'
 
 describe('noop', () => {
   it('不处理参数', () => {
@@ -18,5 +18,19 @@ describe('pass', () => {
     const spy = jest.fn()
     pass(spy)
     expect(spy).not.toHaveBeenCalled()
+  })
+})
+
+describe('passWith', () => {
+  it('不处理参数', () => {
+    const args = [1, 'two', true, null, undefined, {}, []] as const
+    expect(passWith(noop)(...args)).toBe(args[0])
+    expect(passWith(noop)()).toBe(undefined)
+  })
+  it('调用函数', () => {
+    const fn = jest.fn()
+    expect(passWith(fn)(1)).toBe(1)
+    expect(passWith(fn)()).toBe(undefined)
+    expect(fn).toHaveBeenCalledTimes(2)
   })
 })
