1dc5dadb8aa04af348698c16ab2d3a3b47755ead
moushu <moushudyx@163.com>
2024-09-11 01:22:43 +0800

    doc: isArray 文档补全

--- src/is/isArray.ts
+++ src/is/isArray.ts
@@ -1,5 +1,11 @@
 /**
  * 类型守卫，判断给定的值是否为数组(使用内置的 `Array.isArray` 方法)
  * @param value 要判断的值
+ * @example
+ * ```js
+ * isArray([]) // true
+ * isArray("") // false
+ * isArray({ 0: 0, length: 1 }) // false
+ * ```
  */
 export const isArray = /*#__PURE__*/ Array.isArray
