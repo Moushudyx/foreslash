1a3f04b75ad694be0a1c4ebcc4816286ff53e2cb
moushu <moushudyx@163.com>
2024-09-11 14:41:50 +0800

    doc: isObject 文档补全

--- src/is/isObject.ts
+++ src/is/isObject.ts
@@ -1,6 +1,14 @@
 /**
  * 类型守卫，判断给定的值是否为对象(不是 `null`)
  * @param value 要判断的值
+ * @example
+ * ```js
+ * isObject({}) // true
+ * isObject([]) // true
+ * isObject(Object(123)) // true
+ * isObject(123) // false
+ * isObject(null) // false
+ * ```
  */
 export function isObject(value: unknown): value is object {
   return typeof value === 'object' && value !== null
