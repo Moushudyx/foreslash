e264dd8a9aa585325c9bf7ca09c26b7c279417bd
moushu <moushudyx@163.com>
2024-09-26 09:41:42 +0800

    ref: 拆分深拷贝 数组、Set、Map 的方法

---
+++ src/utils/_deepCloneBase.ts
@@ -0,0 +1,47 @@
+type CloneFn = <T>(obj: T, map: Map<any, any>, ...args: any[]) => T
+/**
+ * 深拷贝数组内部实现
+ * @param obj 要拷贝的数组
+ * @param map 缓存对象，用于处理循环引用
+ * @param cloner 深拷贝方法
+ * @param args 深拷贝方法的其他参数
+ */
+export function _cloneArray<T extends Array<any>>(obj: T, map: Map<any, any>, cloner: CloneFn, ...args: any[]): T {
+  const res = obj.slice() as T
+  map.set(obj, res)
+  // 使用经典 for 循环以避免 for...of 或者 forEach 调用迭代器的开销
+  for (let index = 0; index < obj.length; index++) {
+    res[index] = cloner(obj[index], map, ...args);
+  }
+  return res
+}
+/**
+ * 深拷贝 `Map` 内部实现
+ * @param obj 要拷贝的 `Map`
+ * @param map 缓存对象，用于处理循环引用
+ * @param cloner 深拷贝方法
+ * @param args 深拷贝方法的其他参数
+ */
+export function _cloneMap<T extends Map<any, any>>(obj: T, map: Map<any, any>, cloner: CloneFn, ...args: any[]): T {
+  const res = new Map() as T
+  map.set(obj, res)
+  // 使用 forEach 遍历以避免旧版运行环境使用 polyfill 的 Map 时 for...of 不生效的问题
+  obj.forEach((value, key) => {
+    res.set(cloner(key, map, ...args), cloner(value, map, ...args))
+  })
+  return res
+}
+/**
+ * 深拷贝 `Set` 内部实现
+ * @param obj 要拷贝的 `Set`
+ * @param map 缓存对象，用于处理循环引用
+ * @param cloner 深拷贝方法
+ * @param args 深拷贝方法的其他参数
+ */
+export function _cloneSet<T extends Set<any>>(obj: T, map: Map<any, any>, cloner: CloneFn, ...args: any[]): T {
+  const res = new Set() as T
+  map.set(obj, res)
+  // 使用 forEach 遍历以避免旧版运行环境使用 polyfill 的 Set 时 for...of 不生效的问题
+  obj.forEach((item) => res.add(cloner(item, map, ...args)))
+  return res
+}
--- src/utils/_fastClone.ts
+++ src/utils/_fastClone.ts
@@ -1,5 +1,5 @@
 import { isArray, isFunction, isMap, isObject, isPromise, isSet, isWeakMap, isWeakSet } from '../is'
-
+import { _cloneArray, _cloneMap, _cloneSet } from './_deepCloneBase'
 /**
  * 快速深拷贝内部实现
  * - 不考虑各种杂七杂八的实现
@@ -13,16 +13,12 @@ export function _fastClone<T>(obj: T, map: Map<any, any>): T {
   if (map.has(obj)) return map.get(obj)
   // 原始类型(包括 `null`)、函数、`Promise` 处理：直接返回
   if (!isObject(obj) || isFunction(obj) || isWeakMap(obj) || isWeakSet(obj) || isPromise(obj)) return obj
-  //
+  if (isArray(obj)) return _cloneArray(obj, map, _fastClone)
+  if (isMap(obj)) return _cloneMap(obj, map, _fastClone)
+  if (isSet(obj)) return _cloneSet(obj, map, _fastClone)
+  // 其他情况
   let res: T
-  if (isArray(obj)) {
-    // 数组
-    res = obj.slice() as T & any[]
-    map.set(obj, res)
-    obj.forEach((item, index) => {
-      ;(res as T & any[])[index] = _fastClone(item, map)
-    })
-  } else if (obj instanceof Date) {
+  if (obj instanceof Date) {
     // 日期
     res = new Date(obj.valueOf()) as T
     map.set(obj, res)
@@ -30,16 +26,6 @@ export function _fastClone<T>(obj: T, map: Map<any, any>): T {
     // 正则
     res = new RegExp(obj.source, obj.flags) as T
     map.set(obj, res)
-  } else if (isMap(obj)) {
-    // Map
-    res = new Map() as T & Map<any, any>
-    map.set(obj, res)
-    for (const item of obj) (res as T & Map<any, any>).set(_fastClone(item[0], map), _fastClone(item[1], map))
-  } else if (isSet(obj)) {
-    // Set
-    res = new Set() as T & Set<any>
-    map.set(obj, res)
-    obj.forEach((item) => (res as T & Set<any>).add(_fastClone(item, map)))
   } else {
     // 一般对象
     res = {} as T & object
