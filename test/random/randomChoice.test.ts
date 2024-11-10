52948cfc821e5bc6b93b91eb5337a83d5603d093
moushu <moushudyx@163.com>
2024-11-11 00:43:31 +0800

    fix: randomChoice 测试脚本，特殊情况补全

--- test/random/randomChoice.test.ts
+++ test/random/randomChoice.test.ts
@@ -40,4 +40,20 @@ describe('randomChoice', () => {
     expect(resMap.e / testCount).toBeCloseTo(5 / 21, 1)
     expect(resMap.f / testCount).toBeCloseTo(6 / 21, 1)
   })
+  it('权重+类数组测试', () => {
+    const resMap = { a: 0, b: 0, c: 0, d: 0, e: 0, f: 0, g: 0 }
+    const testArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g'] as const
+    const weight = { 0: 1, 1: 2, 2: 3, 3: 4, 4: 5, 5: 6, 6: NaN, awa: 'qwq', foo: 'bar', length: 7 }
+    for (let i = 0; i < testCount; i++) {
+      const res = randomChoice(testArr, weight)
+      resMap[res]++
+    }
+    expect(resMap.a / testCount).toBeCloseTo(1 / 21, 1)
+    expect(resMap.b / testCount).toBeCloseTo(2 / 21, 1)
+    expect(resMap.c / testCount).toBeCloseTo(3 / 21, 1)
+    expect(resMap.d / testCount).toBeCloseTo(4 / 21, 1)
+    expect(resMap.e / testCount).toBeCloseTo(5 / 21, 1)
+    expect(resMap.f / testCount).toBeCloseTo(6 / 21, 1)
+    expect(resMap.g).toBe(0)
+  })
 })
