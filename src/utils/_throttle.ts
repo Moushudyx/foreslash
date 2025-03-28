cde9b655bb7a2b6b474a311b6b29e74d7bfc0e00
moushu <moushudyx@163.com>
2025-03-28 13:39:27 +0800

    fix: 防抖方法执行时应该取最后一次触发时的输入参数

--- src/utils/_throttle.ts
+++ src/utils/_throttle.ts
@@ -1,6 +1,8 @@
 export interface ThrottleOptions {
   /** 节流时触发的最后一次调用是否执行, 默认为否 */
   trailing?: boolean
+  /** 触发的最后一次调用是否使用最后一次的参数, 默认为是 */
+  trailingRunLast?: boolean
   /** 第一次触发是否直接执行, 默认为是 */
   leading?: boolean
 }
@@ -8,6 +10,7 @@ export interface ThrottleOptions {
 /** */
 export function _throttle<T extends any[]>(fn: (...args: T) => any, delay: number, options?: ThrottleOptions) {
   const trailing = options?.trailing ?? false
+  const trailingRunLast = options?.trailingRunLast ?? true
   const leading = options?.leading ?? true
   let timer: null | ReturnType<typeof setTimeout> = null
   let lastTime = 0
@@ -26,13 +29,20 @@ export function _throttle<T extends any[]>(fn: (...args: T) => any, delay: numbe
       const now = Date.now()
       /** 大于等于 0 表示已经过了 delay 毫秒 */
       const timeGap = now - lastTime - delay
+      // timeGap >= 0 表示已经过了节流阈值
       if (timeGap >= 0) {
         lastTime = now
         clearTimer()
       }
+      // 如果配置了触发最后一次调用时使用最后一次的参数, 则清除定时器重新生成一个
+      if (timeGap < 0 && trailing && trailingRunLast && timer) {
+        clearTimer()
+      }
       if (timeGap >= 0 && leading) {
+        // 第一次触发直接执行
         fn.apply(this, args)
       } else if (timeGap >= 0 || (timeGap < 0 && trailing && !timer)) {
+        // 节流时触发的最后一次调用
         timer = setTimeout(
           () => {
             lastTime = Date.now()
