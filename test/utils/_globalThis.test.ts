45dd4294838ba328dcedbbdc8afbad4968f903db
moushu <moushudyx@163.com>
2026-01-31 01:13:11 +0800

    refactor: 重写 getGlobalThis 避免部分环境下调用出现 ReferenceError

--- test/utils/_globalThis.test.ts
+++ test/utils/_globalThis.test.ts
@@ -8,25 +8,39 @@ describe('getGlobalThis', () => {
 
 describe('getGlobalThis 当 self 不存在时', () => {
   beforeEach(() => {
-    jest.spyOn(global, 'self', 'get').mockImplementation(() => undefined as any);
-  });
+    jest.spyOn(global, 'self', 'get').mockImplementation(() => undefined as any)
+  })
   afterEach(() => {
-    jest.restoreAllMocks();
-  });
+    jest.restoreAllMocks()
+  })
   it('返回 globalThis', () => {
-    expect(getGlobalThis()).toBe(globalThis);
-  });
+    expect(getGlobalThis()).toBe(globalThis)
+  })
 })
 
 describe('getGlobalThis 当 self 和 window 不存在时', () => {
   beforeEach(() => {
-    jest.spyOn(global, 'self', 'get').mockImplementation(() => undefined as any);
-    jest.spyOn(global, 'window', 'get').mockImplementation(() => undefined as any);
-  });
+    jest.spyOn(global, 'self', 'get').mockImplementation(() => undefined as any)
+    jest.spyOn(global, 'window', 'get').mockImplementation(() => undefined as any)
+  })
   afterEach(() => {
-    jest.restoreAllMocks();
-  });
+    jest.restoreAllMocks()
+  })
   it('返回 globalThis', () => {
-    expect(getGlobalThis()).toBe(globalThis);
-  });
+    expect(getGlobalThis()).toBe(globalThis)
+  })
 })
+// global 和 globalThis 在 jest 环境下无法被 mock
+// describe('getGlobalThis 当 self 和 window、global 不存在时', () => {
+//   beforeEach(() => {
+//     jest.spyOn(global, 'self', 'get').mockImplementation(() => undefined as any)
+//     jest.spyOn(global, 'window', 'get').mockImplementation(() => undefined as any)
+//     jest.spyOn(global, 'global', 'get').mockImplementation(() => undefined as any)
+//   })
+//   afterEach(() => {
+//     jest.restoreAllMocks()
+//   })
+//   it('返回 globalThis', () => {
+//     expect(getGlobalThis()).toBe(globalThis)
+//   })
+// })
