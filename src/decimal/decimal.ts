9bcaf6ab911a5ab9a0298de3d0f58a6f825683ca
moushu <moushudyx@163.com>
2026-03-13 14:42:19 +0800

    wip: 进一步的设计

--- src/decimal/decimal.ts
+++ src/decimal/decimal.ts
@@ -2,32 +2,36 @@
 大数字处理方案
 
 1. 储存方式, 类似 IEEE 754 浮点数的存储方式
-  - 符号位
-  - 指数位
+  - 符号位, -1 0 1 分别代表负数、零、正数
+  - 指数位, 万进制, 代表尾数需要乘以多少个 10000
   - 尾数位
-    - 数字数组
-    - 十进制
+    - 数字数组, 万进制
+    - 比如 [1, 34, 5678] 代表 1*10^8 + 34*10^4 + 5678*10^0 = 100000000 + 340000 + 5678 = 100345678
+  - 类型标识, 区分 普通数字 `NaN` `Infinity` `-Infinity`
+  - 最终值 = 符号位 * 尾数位 * 10000^指数位
 2. 运算方式
-  - 加法 plus add
-  - 减法 minus sub
-  - 乘法 multiply mul
-  - 除法 dividedBy div
-  - 取模 modulo mod
-  - 幂运算 power pow
+  - 加法 plus 别名 add
+  - 减法 minus 别名 sub
+  - 乘法 multiply 别名 mul
+  - 除法 dividedBy 别名 div
+  - 取模 modulo 别名 mod
+  - 幂运算 power 别名 pow
   - 操作优化
-    - 不可变对象, 每次运算返回一个新的对象
-    - （实际实现起来非常麻烦而且容易出现问题, 不做实现）不直接计算, 直到转换为数字时才进行计算——这一点需要实现不可变对象
+    - 不可变对象, 每次运算操作返回一个新的对象
+    - （实际实现起来非常麻烦而且容易出现问题, 目前先不做实现）不直接计算, 直到转换为数字时才进行计算——这一点需要实现不可变对象
 3. 其他操作
   - 比较大小
-    - 等于 equals equalTo eq
-    - 大于 greaterThan gt
-    - 小于 lessThan lt
-    - 大于等于 greaterThanOrEqual gte
-    - 小于等于 lessThanOrEqual lte
-  - 取反
-  - 取绝对值
-  - 四舍五入等修约操作
+    - 等于 equals equalTo 别名 eq
+    - 大于 greaterThan 别名 gt
+    - 小于 lessThan 别名 lt
+    - 大于等于 greaterThanOrEqual 别名 gte
+    - 小于等于 lessThanOrEqual 别名 lte
   - isNaN、isFinite、isInteger 等检测操作
+  - 取反 negated 别名 neg
+  - 取绝对值 absoluteValue 别名 abs
+  - 四舍五入等修约操作
+  - 操作优化
+    - 不可变对象, 每次运算操作返回一个新的对象
 4. 输入
   - 数字
   - bigint
@@ -41,9 +45,9 @@
   - 转换为数字（如果在安全范围内） toNumber
   - 转换为指定格式（科学计数法、定点数、其他进制等）
     - 在 format、scientificNotation 等方法内做适配
-    - 二进制 toBinary toBin
-    - 八进制 toOctal toOct
-    - 十六进制 toHexadecimal toHex
+    - 二进制 toBinary 别名 toBin
+    - 八进制 toOctal 别名 toOct
+    - 十六进制 toHexadecimal 别名 toHex
 */
 import type { baseForeNumber, ForeNumberConstructor } from './decimal.d'
 /** 大数字处理方案 */
